1a4845a2f16228152c7f8f36cd361c6415e9c994
JuhanLim <dlawhks@naver.com>
2024-07-02 16:21:57 +0900

    static

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = "django-insecure-v0(w91=lnx#d^^yh)z3@mlrunsq@@dmv9y()6^re5rl&6-*%+*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "175.45.204.163",
+]
 
 
 # Application definition
@@ -116,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
